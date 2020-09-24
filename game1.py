cb9fbb739f1428c0f181d9f58d13fef97db17707
eskinders17 <22ehaileyesus@athenian.org>
2020-09-24 09:49:14 +0000

    Finished

--- game1.py
+++ game1.py
@@ -1,11 +1,31 @@
-'''
-Use this file to write an "open" version of the game (no test code or defined format). This will be translated into a testable program later. Use the reponse statements provided to make that transition easier.
 
-"Your number is too high."
-"Your number is too low."
-"Your number is out of range."
-"I'm sorry you are giving up!"
-"I'm sorry, you are out of guesses."
 
-'''
-# Collaborators: 
+from random import randint
+guesses = 1
+number = randint(1,10)
+
+x = int(input("Guess a number between 1 and 10: "))
+
+while x != number:
+    
+    if x < number:
+        print("Your number is too low.")
+        x = int(input("Please guess again: "))
+        guesses = guesses + 1
+    elif x > number:
+        print("Your number is too high.") 
+        x = int(input("PLease guess again: ")) 
+        guesses = guesses + 1
+
+    
+    print("you are out of guesses")    
+          
+print()
+print("Congrats, you guessed the number! ")
+print("It only took you", guesses, "guesses!")
+# Collaborators: none
+
+
+
+
+
