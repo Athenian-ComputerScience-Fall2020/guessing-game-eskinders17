c077b7cafcbfd569867a44e57b53c29fb87fb777
eskinders17 <22ehaileyesus@athenian.org>
2020-09-29 15:35:08 +0000

    Finished

--- game1.py
+++ game1.py
@@ -3,24 +3,33 @@
 from random import randint
 guesses = 1
 number = randint(1,10)
+guess_limit = 5
+out_of_guesses = False
 
 x = int(input("Guess a number between 1 and 10: "))
 
-while x != number:
-    
-    if x < number:
+while x != number and not(out_of_guesses):
+
+    if guesses < guess_limit:
+
+     if x < number:
         print("Your number is too low.")
         x = int(input("Please guess again: "))
         guesses = guesses + 1
-    elif x > number:
+     elif x > number:
         print("Your number is too high.") 
         x = int(input("PLease guess again: ")) 
         guesses = guesses + 1
+    else:
+        out_of_guesses = True  
    
-          
-print()
-print("Congrats, you guessed the number! ")
-print("It only took you", guesses, "guesses!")
+if out_of_guesses:
+    print()
+    print("Game over, you are out of guesses")
+else:
+ print()
+ print("Congrats, you guessed the number! ")
+ print("It only took you", guesses, "guesses!")
 # Collaborators: none
 
 
